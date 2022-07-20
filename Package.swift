5a67c69d99d4dd7ea33ca5ddb2973f7d80cbdcdd
kelvin13 <kelvin13ma@gmail.com>
2022-07-19 19:11:48 -0500

    initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package:Package = .init(
+    name: "swift-system-extras",
+    products: 
+    [
+         .library(name: "SystemExtras",  targets: ["SystemExtras"]),
+    ],
+    dependencies: 
+    [
+        .package(url: "https://github.com/apple/swift-system.git",  from: "1.2.1"),
+    ],
+    targets: 
+    [
+        .target(name: "SystemExtras", 
+            dependencies: 
+            [
+                .product(name: "SystemPackage", package: "swift-system"),
+            ]),
+    ]
+)
